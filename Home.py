d57a235bd614f5cbd5d231dfb6bbae0121af853e
ailainalan <thiniem1982bp@gmail.com>
2022-09-18 15:12:28 +0700

    haha

--- Home.py
+++ Home.py
@@ -2,7 +2,8 @@ import streamlit as st
 # from PIL import Image
 
 
-st.title('Bin Khùng')
+st.title('Bin Đẹp trai')
 st.subheader('đây là ảnh:')
+
 # image = Image.open('Tesla-1 (1).jpg')
 # st.image(image, caption='Xe Hơi Tesla')
