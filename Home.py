a4edb21a71dd811d40dfe53a86fd9e4f79450208
ailainalan <thiniem1982bp@gmail.com>
2022-09-18 15:09:32 +0700

    COCAINIT

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# from PIL import Image
+
+
+st.title('Bin Khùng')
+st.subheader('đây là ảnh:')
+# image = Image.open('Tesla-1 (1).jpg')
+# st.image(image, caption='Xe Hơi Tesla')
